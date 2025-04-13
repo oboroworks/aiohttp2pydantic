8b0712e38f71a5306f2675f60de4393ab1ffe801
Guido Dassori <guido.dassori@gmail.com>
2025-04-14 03:05:55 +0700

    update missing code

--- aiohttp2pydantic/aiohttp2pydantic/__init__.py
+++ aiohttp2pydantic/aiohttp2pydantic/__init__.py
@@ -1,12 +1,10 @@
 from functools import wraps
 from pydantic import BaseModel
-from typing import Type, TypeVar
+from typing import Type
 from aiohttp import web
 
-T = TypeVar("T", bound=BaseModel)
-
 class PydanticAiohttpRequest:
-    def __init__(self, request: web.Request, data: T):
+    def __init__(self, request: web.Request, data: BaseModel):
         self.request = request
         self.data = data
 
@@ -14,8 +12,17 @@ def aiohttp_to_pydantic(model: Type[BaseModel]):
     def decorator(handler):
         @wraps(handler)
         async def wrapper(request: web.Request):
-            payload = await request.json()
-            validated = model(**payload)
+            method = request.method
+            if method == 'GET':
+                data = dict(request.query)
+            elif request.content_type == 'application/json':
+                data = await request.json()
+            elif request.content_type == 'application/x-www-form-urlencoded':
+                data = await request.post()
+            else:
+                data = await request.read()  # fallback
+
+            validated = model(**data)
             pyd_request = PydanticAiohttpRequest(request, validated)
             return await handler(pyd_request)
 
