2004fc87bda393eafd5ed31a84ad1f10f509bdc4
Guido Dassori <guido.dassori@gmail.com>
2025-04-14 12:50:33 +0700

    return validation errors

--- aiohttp2pydantic/aiohttp2pydantic/__init__.py
+++ aiohttp2pydantic/aiohttp2pydantic/__init__.py
@@ -1,15 +1,13 @@
 from functools import wraps
-from pydantic import BaseModel
-from typing import Type
+from typing import TypeVar, Callable, Type
 from aiohttp import web
-
-from typing import TypeVar, Callable
+from pydantic import BaseModel, ValidationError as PydanticValidationError
 
 T = TypeVar("T", bound=BaseModel)
 
 
 class PydanticAiohttpRequest:
-    def __init__(self, request: web.Request, data: Type[T]):
+    def __init__(self, request: web.Request, data: T):
         self.request = request
         self.data = data
 
@@ -18,19 +16,33 @@ def aiohttp_to_pydantic(model: Type[T]) -> Callable:
     def decorator(handler):
         @wraps(handler)
         async def wrapper(request: web.Request):
-            method = request.method
-            if method == 'GET':
-                data = dict(request.query)
-            elif request.content_type == 'application/json':
-                data = await request.json()
-            elif request.content_type == 'application/x-www-form-urlencoded':
-                data = await request.post()
-            else:
-                data = await request.read()  # fallback
+            try:
+                if request.method == 'GET':
+                    data = dict(request.query)
+                elif request.content_type == 'application/json':
+                    data = await request.json()
+                elif request.content_type == 'application/x-www-form-urlencoded':
+                    data = await request.post()
+                else:
+                    data = await request.read()  # fallback
 
-            validated = model(**data)
-            pyd_request = PydanticAiohttpRequest(request, validated)
-            return await handler(pyd_request)
+                validated = model(**data)
+                pyd_request = PydanticAiohttpRequest(request, validated)
+                return await handler(pyd_request)
+
+            except PydanticValidationError as e:
+                return web.json_response(
+                    {
+                        "errors": [
+                            {
+                                "loc": err['loc'],
+                                "msg": err['msg'],
+                                "type": err['type']
+                            } for err in e.errors()
+                        ]
+                    },
+                    status=400
+                )
 
         return wrapper
     return decorator
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aiohttp2pydantic',
-    version='0.11',
+    version='0.12',
     license='MIT',
     author="Oboro Works LLC",
     author_email='dev@oboroworks.com',
